82be4cb3dc3e761d98d5d60c1b32c656f1c67ea5
Mário Neto <marioo.netoo3@gmail.com>
2024-08-01 21:03:20 -0300

    day one

---
+++ calcporcent.php
@@ -0,0 +1,6 @@
+<?php
+$porcent = 0.1; //10%
+$numExample = 532;
+$result = $numExample * $porcent;
+
+echo "O resultado é: " . $result;
---
+++ parorimpar.php
@@ -0,0 +1,10 @@
+<?php
+
+$num = 569;
+$par = $num / 2 == 0;
+
+if ($par) {
+    echo "o número $num é PAR";
+} else {
+    echo "o número é IMPAR";
+}
